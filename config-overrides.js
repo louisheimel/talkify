84db6b7cb37d432c636ba5e12f60d11e22834754
Louis Heimel <louisheimel@Louiss-MacBook-Pro.local>
2019-01-21 13:36:20 -0600

    add comment which links to coolors theme

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+// coolors theme: https://coolors.co/ef767a-456990-49beaa-49dcb1-eeb868
 
 module.exports = override(
 	fixBabelImports('import', {
