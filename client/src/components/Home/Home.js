7ec2cf07ce210fe1c8729e6365bfe717cf1c3f2e
louisheimel <louisheimel1@gmail.com>
2019-01-23 12:53:31 -0600

    add controlled textarea which submits to messages array

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
 
-import { Layout } from "antd";
+import { Layout, Input } from "antd";
 
 import Toggler from "./Toggler";
 import Threads from "./Threads";
+
+const { TextArea } = Input;
 const { Sider, Content } = Layout;
 
 class Home extends Component {
-  state = {};
+  state = {
+    inputValue: "",
+    messages: []
+  };
   toggle = () => {
     this.setState(prevState => ({
       ...prevState,
       collapsed: !prevState.collapsed
     }));
+  };
+
+  handleMessage = e => {
+    this.setState(
+      {
+        messages: [...this.state.messages, e.target.value],
+        inputValue: ""
+      },
+      () => console.log(this.state.messages, this.state.inputValue)
+    );
+  };
+
+  handleInputChange = e => {
+    this.setState(
+      {
+        ...this.state,
+        inputValue: e.target.value
+          .split("")
+          .filter(e => e !== "\n")
+          .join("")
+      },
+      () =>
+        console.log(
+          this.state.inputValue,
+          " from inside handleInputChange method"
+        )
+    );
   };
   render() {
     const { collapsed } = this.state;
@@ -20,6 +52,21 @@ class Home extends Component {
     const siderStyles = { color: "#fff", fontSize: "24px", userSelect: "none" };
     const layoutStyles = {
       position: "relative"
+    };
+
+    const contentStyles = {
+      display: "flex",
+      justifyContent: "center",
+      position: "relative",
+      width: "100%",
+      boxSizing: "border-box"
+    };
+
+    const inputStyles = {
+      margin: "0 30px",
+      width: "100%",
+      resize: "none",
+      alignSelf: "flex-end"
     };
 
     const threads = {
@@ -34,7 +81,15 @@ class Home extends Component {
           <Threads threads={threads} showList={!collapsed} />
           <Toggler toggle={this.toggle} collapsed={collapsed} />
         </Sider>
-        <Content>Main Content</Content>
+        <Content style={contentStyles}>
+          <TextArea
+            style={inputStyles}
+            autosize
+            onPressEnter={this.handleMessage}
+            onChange={this.handleInputChange}
+            value={this.state.inputValue}
+          />
+        </Content>
       </Layout>
     );
   }
