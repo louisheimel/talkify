efa24700aba9825f8c1a2a012f6a87c2bd45b57b
Louis Heimel <louisheimel@Louiss-MacBook-Pro.local>
2019-01-27 12:24:31 -0600

    add todos

--- client/src/components/Home/ThreadList.js
+++ client/src/components/Home/ThreadList.js
@@ -4,6 +4,7 @@ import ThreadListItem from "./ThreadListItem";
 class ThreadList extends Component {
   render() {
     const { threads, threadName, showList } = this.props;
+    console.log(threads, threadName, showList);
 
     const ulStyles = {
       paddingLeft: "20px"
@@ -14,9 +15,28 @@ class ThreadList extends Component {
     };
 
     const threadListItems = threads && threads.map(ThreadListItem);
-
-    return (
+    return threadName === "Work" ? (
       <Fragment>
+        {/* TODO: Implement dropdown menu to change workspaces here */}
+
+        <p
+          style={
+            showList
+              ? pStyles
+              : {
+                  ...pStyles,
+                  fontSize: "12px",
+                  marginTop: "30px",
+                  fontWeight: "700"
+                }
+          }
+        >
+          Work
+        </p>
+      </Fragment>
+    ) : (
+      <Fragment>
+        {/* TODO: render channel and dm thread here */}
         <p
           style={
             showList
