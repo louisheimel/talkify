2e4006c44084696b28918f480f8df3eecef9f911
Louis Heimel <louisheimel@Louiss-MBP.attlocal.net>
2019-01-27 13:15:29 -0600

    add hover menu for workspace

--- client/src/components/Home/ThreadList.js
+++ client/src/components/Home/ThreadList.js
@@ -1,6 +1,18 @@
 import React, { Component, Fragment } from "react";
+import { Dropdown, Menu } from "antd";
 import ThreadListItem from "./ThreadListItem";
-
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a href="http://www.alipay.com/">1st menu item</a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a href="http://www.taobao.com/">2nd menu item</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3">3rd menu item</Menu.Item>
+  </Menu>
+);
 class ThreadList extends Component {
   render() {
     const { threads, threadName, showList } = this.props;
@@ -16,25 +28,28 @@ class ThreadList extends Component {
 
     const threadListItems = threads && threads.map(ThreadListItem);
     return threadName === "Work" ? (
-      <Fragment>
-        {/* TODO: Implement dropdown menu to change workspaces here */}
-
-        <p
-          style={
-            showList
-              ? pStyles
-              : {
-                  ...pStyles,
-                  fontSize: "12px",
-                  marginTop: "30px",
-                  fontWeight: "700"
-                }
-          }
-        >
-          Work
-        </p>
-      </Fragment>
+      <Dropdown overlay={menu} trigger={["hover"]}>
+        <p>Workspace</p>
+      </Dropdown>
     ) : (
+      // <Fragment>
+      //   {/* TODO: Implement dropdown menu to change workspaces here */}
+
+      //   <p
+      //     style={
+      //       showList
+      //         ? pStyles
+      //         : {
+      //             ...pStyles,
+      //             fontSize: "12px",
+      //             marginTop: "30px",
+      //             fontWeight: "700"
+      //           }
+      //     }
+      //   >
+      //     Work
+      //   </p>
+      // </Fragment>
       <Fragment>
         {/* TODO: render channel and dm thread here */}
         <p
