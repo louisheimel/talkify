cdbc7ffd56918f3268de668d8d2304c73c5f2649
louisheimel <louisheimel1@gmail.com>
2019-01-30 12:15:34 -0600

    show current message thread

--- client/src/redux/reducers/selectors/threadSelector.js
+++ client/src/redux/reducers/selectors/threadSelector.js
@@ -1,20 +1,10 @@
 import { createSelector } from "reselect";
 
-import getWorkspace from "./currentWorkspaceSelector";
-import getCurrentThread from "./currentChannelSelector";
 import getAllMessages from "./currentMessagesSelector";
 
-// const getWorkspace = store => store.threads.workspace.current;
-// const getCurrentThread = store => store.threads.currentChannel;
-// const getAllMessages = store => store.threads.messages;
-
 const getMessages = createSelector(
-  [getWorkspace, getCurrentThread, getAllMessages],
-  (workspace, currentThread, allMessages) =>
-    (allMessages &&
-      allMessages[workspace] &&
-      allMessages[workspace][currentThread]) ||
-    []
+  [getAllMessages],
+  allMessages => allMessages.messages
 );
 
 export default getMessages;
